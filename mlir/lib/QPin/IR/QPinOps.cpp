63bc98107a1410cb6f3f48c087bd4f3b4dd3690a
Matthias Reumann <matthias.reumann@tum.de>
2025-07-24 11:17:35 +0200

    Add kernel verifier for QPin Dialect

--- mlir/lib/QPin/IR/QPinOps.cpp
+++ mlir/lib/QPin/IR/QPinOps.cpp
@@ -1,6 +1,8 @@
 #include "QPin/IR/QPinDialect.h"
 
+#include "mlir/Dialect/Arith/IR/Arith.h"            // IWYU pragma: keep
 #include "mlir/Dialect/Func/IR/FuncOps.h"           // IWYU pragma: keep
+#include "mlir/Dialect/SCF/IR/SCF.h"                // IWYU pragma: keep
 #include "mlir/IR/Attributes.h"                     // IWYU pragma: keep
 #include "mlir/IR/Builders.h"                       // IWYU pragma: keep
 #include "mlir/IR/BuiltinTypes.h"                   // IWYU pragma: keep
@@ -19,6 +21,79 @@
 namespace aqomplice {
 namespace qpin {
 using namespace mlir;
+
+/**
+ * @brief Verify that the operand qubits of an unitary operation aren't used
+ * after measurement.
+ */
+template <typename UnitaryOp>
+bool usedAfterMeasurement(const llvm::DenseSet<Value> &measured,
+                          const Operation &op) {
+  if (auto u = mlir::dyn_cast<UnitaryOp>(op)) {
+    bool used = measured.find(u.getTarget()) != measured.end();
+    if (auto ctrl = u.getControl()) {
+      used |= measured.find(ctrl) != measured.end();
+    }
+    return used;
+  }
+
+  return false;
+}
+
+/**
+ * @brief Verify that the operand qubits of an unitary operation aren't used
+ * after measurement.
+ */
+template <>
+bool usedAfterMeasurement<qpin::SwapOp>(const llvm::DenseSet<Value> &measured,
+                                        const Operation &op) {
+  if (auto u = mlir::dyn_cast<qpin::SwapOp>(op)) {
+    return measured.find(u.getA()) != measured.end() &&
+           measured.find(u.getB()) != measured.end();
+  }
+  return false;
+}
+
+/**
+ * @brief Verify that the kernel fulfills QIR's base profile.
+ */
+llvm::LogicalResult KernelOp::verifyRegions() {
+  llvm::DenseSet<Value> measured{};
+  for (auto &op : getBody().getOps()) {
+    if (isa<scf::SCFDialect>(op.getDialect())) {
+      return emitOpError() << "No classical control flow elements are allowed "
+                              "in a kernel (as of now).";
+    }
+
+    if (isa<arith::ArithDialect>(op.getDialect())) {
+      if (!isa<arith::ConstantOp>(op)) {
+        return emitOpError() << "No arithmetic or other calculations may be "
+                                "performed with classical values.";
+      }
+    }
+
+    if (!isa<qpin::QPinDialect>(op.getDialect())) {
+      continue;
+    }
+
+    if (auto measureOp = mlir::dyn_cast<qpin::MeasureOp>(op)) {
+      measured.insert(measureOp.getQubit());
+    }
+
+    if (usedAfterMeasurement<qpin::HOp>(measured, op) ||
+        usedAfterMeasurement<qpin::XOp>(measured, op) ||
+        usedAfterMeasurement<qpin::YOp>(measured, op) ||
+        usedAfterMeasurement<qpin::ZOp>(measured, op) ||
+        usedAfterMeasurement<qpin::SOp>(measured, op) ||
+        usedAfterMeasurement<qpin::TOp>(measured, op) ||
+        usedAfterMeasurement<qpin::SwapOp>(measured, op)) {
+      return emitOpError() << "Once a qubit is measured, nothing further "
+                              "will be done with it other than releasing it.";
+    }
+  }
+
+  return mlir::success();
+}
 
 void KernelOp::build(OpBuilder &builder, OperationState &state,
                      llvm::StringRef name, FunctionType type,
