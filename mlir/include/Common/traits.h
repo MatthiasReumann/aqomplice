da3e117a0820fac7fdd1c6902a6a86c9bade139c
Matthias Reumann <matthias.reumann@tum.de>
2025-08-01 15:51:06 +0200

    Add InsideQPUKernel trait

--- mlir/include/Common/traits.h
+++ mlir/include/Common/traits.h
@@ -1,6 +1,7 @@
 #ifndef AQOMPLICE_COMMON_TRAITS_H
 #define AQOMPLICE_COMMON_TRAITS_H
 
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/OpDefinition.h"
 
 namespace mlir {
@@ -11,6 +12,25 @@ class UnitaryTrait : public TraitBase<ConcreteType, UnitaryTrait> {};
 
 template <typename ConcreteType>
 class HermitianTrait : public TraitBase<ConcreteType, HermitianTrait> {};
+
+template <typename ConcreteType>
+class InsideQPUKernelTrait
+    : public OpTrait::TraitBase<ConcreteType, InsideQPUKernelTrait> {
+public:
+  static LogicalResult verifyTrait(Operation *op) {
+    const auto p = op->getParentOp();
+    if (llvm::isa_and_nonnull<func::FuncOp>(p) && p->hasAttr("qpu.kernel")) {
+      if (auto k = p->getAttrOfType<BoolAttr>("qpu.kernel")) {
+        if (k.getValue()) {
+          return success();
+        }
+      }
+    }
+
+    return op->emitOpError() << "expects parent op to be func.func with "
+                                "attribute 'qpu.kernel' set to true";
+  }
+};
 
 } // namespace OpTrait
 } // namespace mlir
